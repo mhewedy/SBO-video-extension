301681667d5b13a54cebd5f57fe417dcd79e52ec
Muhammad Hewedy <mhewedy@gmail.com>
2016-07-23 22:34:09 +0300

    restructure the code and use jqXHR.always method

--- content.js
+++ content.js
@@ -1,38 +1,31 @@
 (function() {
 
-    $('a[data-clip-ref]').each(function() {
-        drawDownloadButton($(this).attr('data-clip-ref'));
-    });
-
-})();
-
-function drawDownloadButton(referenceId) {
-    console.log('referenceId: ' + referenceId);
-
-    // 1. define the 3 variables to be used in the url to get entryId
+    // 1. define the 2 variables to be used in the url to get entryId
     var wid, uiconfId;
 
-    // 2. use jquery to find 3 variables
+    // 2. use jquery to find the 2 variables
     var jsSrc = $('#metadata_flashactive > div > div.sdwBoxContent > div.brightcove_video > script').attr('src');
     console.log('jsSrc: ' + jsSrc);
     var a = jsSrc.match(/\d+/g);
     wid = a[0];
     uiconfId = a[2];
 
-    // 3. compose the url to get entryId
+    // 3. for each referenceId
+    $('a[data-clip-ref]').each(function() {
+        drawDownloadButton(wid, uiconfId, $(this).attr('data-clip-ref'));
+    });
+
+})();
+
+function drawDownloadButton(wid, uiconfId, referenceId) {
+
     var url = 'http://cdnapi.kaltura.com/html5/html5lib/v2.35/mwEmbedFrame.php?&wid=_' + wid +
         '&uiconf_id=' + uiconfId + '&flashvars[referenceId]=' + referenceId + '&callback=o';
     console.log('url: ' + url);
 
-    // 4. call the url to get json object and parse it to get downloadUrl
-
-    $.get(url)
-        .done(function(o) {
-            handle(o, referenceId);
-        })
-        .fail(function(o) {
-            handle(o, referenceId);
-        });
+    $.get(url).always(function(o) {
+        handle(o, referenceId);
+    });
 }
 
 function handle(o, referenceId) {
