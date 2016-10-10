f82cb8bfc8e657e5c5f67e82450979e955752a06
Muhammad Hewedy <mhewedy@gmail.com>
2016-10-10 21:05:42 +0300

    4 Links not Created on Learning path

--- core/new.js
+++ core/new.js
@@ -3,11 +3,14 @@ function startNew() {
     var techbusBaseUrl = 'https://techbus.safaribooksonline.com/';
     var pathArr = window.location.pathname.split('/');
     var courseId = pathArr[pathArr.length - 2];
+	
+	var isFlatToC = $('#container  ol > li > ol > li > a').length == 0
+	var domList = isFlatToC ? $('#container  ol > li > a') : $('#container  ol > li > ol > li > a');
 
     $.get(techbusBaseUrl + courseId).always(function(html) {
         start({
             drawFunction: draw,
-            domList: $('#container  ol > li > ol > li > a')
+            domList: domList
         }, html);
     });
 }
